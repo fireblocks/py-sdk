ac89e8ff4ea94542f744e115864955d249905fe8
SCFireblocks <126114220+SCFireblocks@users.noreply.github.com>
2023-12-01 13:06:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name=NAME,
-    version="1.0.0",
+    version="v2.0.3-beta",
     description="Fireblocks Auto Generated SDK",
     author="Fireblocks",
     author_email="support@fireblocks.com",
